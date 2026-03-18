9fe2ea5706a47bed2d8f924e3d7d170b9491bd23
Stepa <stefan.mahotkin2@gmail.com>
2026-03-18 23:37:12 +0700

    Added controller ScheduleController.cs in Contollers folders

---
+++ CollegeScheduleMahotkin/Controllers/ScheduleController.cs
@@ -0,0 +1,19 @@
+﻿using CollegeScheduleMahotkin.Data;
+using CollegeScheduleMahotkin.Models;
+using CollegeScheduleMahotkin.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeScheduleMahotkin.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+    }
+}

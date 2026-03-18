377a1ae936b4e5e009c09958a6b02166ce069017
Stepa <stefan.mahotkin2@gmail.com>
2026-03-18 23:45:59 +0700

    Implementing the GET method in ScheduleController.cs

--- CollegeScheduleMahotkin/Controllers/ScheduleController.cs
+++ CollegeScheduleMahotkin/Controllers/ScheduleController.cs
@@ -8,12 +8,22 @@ namespace CollegeScheduleMahotkin.Controllers
 {
     [ApiController]
     [Route("api/schedule")]
-    public class ScheduleController
+    public class ScheduleController : ControllerBase
     {
         private readonly IScheduleService _service;
         public ScheduleController(IScheduleService service, AppDbContext db)
         {
             _service = service;
+        }
+
+        // GET: api/schedule/group/{groupId}?start=2026-03-16&end=2026-04-15
+        [HttpGet("group/{groupId}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            // Вызываем бизнес-логику из сервиса
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+            // Возвращаем результат со статусом 200 OK
+            return Ok(result);
         }
     }
 }

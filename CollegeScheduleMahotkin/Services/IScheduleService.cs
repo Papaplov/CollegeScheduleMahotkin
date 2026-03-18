426aa6b29b9fc4a85d120309c6ccbcf8886434b2
Stepa <stefan.mahotkin2@gmail.com>
2026-03-18 23:29:54 +0700

    In the Services folder, create the IScheduleService.cs and ScheduleService.cs files

---
+++ CollegeScheduleMahotkin/Services/IScheduleService.cs
@@ -0,0 +1,9 @@
+﻿using CollegeScheduleMahotkin.DTO;
+
+namespace CollegeScheduleMahotkin.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime startDate, DateTime endDate);
+    }
+}
